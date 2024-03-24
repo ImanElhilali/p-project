4550ce849fba57284a16cfcee2bfad4a4b34894f
ImanElhilali <imanelhilali1234@gmail.com>
2024-03-24 14:37:33 +0200

    kkk

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://p-project-api.onrender.com',
+      target: 'https://p-project-api.onrender.com/',
       changeOrigin: true,
     })
   )
