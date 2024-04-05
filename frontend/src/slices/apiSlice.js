0f80ba81c30e8e4a9316c4143f9bc9180510950b
ImanElhilali <imanelhilali1234@gmail.com>
2024-04-05 14:15:03 +0200

    o

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,6 +3,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 const baseQuery = fetchBaseQuery({
   // baseUrl: '',
   baseUrl: `https://p-project-api.onrender.com/`,
+  credentials: 'include',
 })
 
 export const apiSlice = createApi({
