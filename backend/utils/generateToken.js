68170196f3b5224d6ca4ccdc7531f80b321a89e9
ImanElhilali <imanelhilali1234@gmail.com>
2024-04-05 10:39:34 +0200

    another change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
-    domain: 'https://p-project.onrender.com',
+    domain: 'https://p-project-api.onrender.com',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   })
 }
